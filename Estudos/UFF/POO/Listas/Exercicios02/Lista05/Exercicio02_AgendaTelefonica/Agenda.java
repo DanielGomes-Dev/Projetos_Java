5e3a27dc118823514d55f416f1b90f9a3ba7747e
unknown <danielgomes1@live.com>
2026-05-13 20:06:14 -0300

    exercici_agenda

---
+++ Estudos/UFF/POO/Listas/Exercicios02/Lista05/Exercicio02_AgendaTelefonica/Agenda.java
@@ -0,0 +1,40 @@
+package Lista05.Exercicio02_AgendaTelefonica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Agenda {
+
+    private List<Contato> lista_contato = new ArrayList<>();
+
+    public Agenda(){
+        System.out.println("Agenda iniciada!");
+    }
+
+    public void add_contato(Contato contato){
+        this.lista_contato.add(contato);
+        System.out.println("Contato Adicionado Com Sucesso");
+
+    }
+
+    public void imprime_contatos(){
+        for (Contato contato : lista_contato) {
+            System.out.printf("%s - %s%n", contato.name, contato.number);
+        }
+    }
+
+    public static void main (String args[]){
+        Contato contato_01 = new Contato("Daniel Gomes", "021 98888-8888");
+        Contato contato_02 = new Contato("Maria Betania", "021 98888-8889");
+        
+        Agenda agenda = new Agenda();
+        agenda.add_contato(contato_01);
+        agenda.add_contato(contato_02);
+
+        agenda.imprime_contatos();
+
+
+    }
+}
+
---
+++ Estudos/UFF/POO/Listas/Exercicios02/Lista05/Exercicio02_AgendaTelefonica/Contato.java
@@ -0,0 +1,11 @@
+package Lista05.Exercicio02_AgendaTelefonica;
+
+public class Contato {
+    public String name;
+    public String number;
+
+    public Contato(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+}
