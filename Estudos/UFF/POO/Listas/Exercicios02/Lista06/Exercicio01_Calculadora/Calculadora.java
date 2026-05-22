11e9b5033fc712adaff8f5c80cf78b1667b0aa8d
unknown <danielgomes1@live.com>
2026-05-22 17:40:37 -0300

    refact calculadora

--- Estudos/UFF/POO/Listas/Exercicios02/Lista06/Exercicio01_Calculadora/Calculadora.java
+++ Estudos/UFF/POO/Listas/Exercicios02/Lista06/Exercicio01_Calculadora/Calculadora.java
@@ -1,20 +1,15 @@
 public class Calculadora {
     
-    // Atributo privado para garantir o encapsulamento.
-    // Assim, impedimos que classes externas modifiquem o valor da memória diretamente.
     private double memoria;
 
-    // Construtor: Método especial usado para inicializar a instância da classe.
     public Calculadora() {
-        this.memoria = 0.0; // Estado inicial da memória é 0
+        this.memoria = 0.0;
     }
 
-    // Método de Acesso (Getter) para leitura segura do atributo.
     public double getMemoria() {
         return this.memoria;
     }
 
-    // Comportamentos / Operações matemáticas
     public void somar(double valor) {
         this.memoria += valor;
     }
@@ -28,7 +23,6 @@ public class Calculadora {
     }
 
     public void dividir(double valor) {
-        // Validação adequada: protege a memória de uma alteração inválida (divisão por zero)
         if (valor == 0) {
             System.out.println("Erro: Não é possível realizar divisão por zero. A operação foi cancelada.");
         } else {
